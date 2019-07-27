0d863afbcf340f341c38126394d097c2b33744bb
Houjun Liu <hliu@shabang.cf>
2019-07-27 16:52:49 +0800

    Added Copyright Statement ~Created~ Nada ~Fixed~ 1. Added copyright statements ~TODO~ None thus far.

---
+++ api/__init__.py
@@ -0,0 +1,9 @@
+'''
+Bada Bing, Bada Boom
+The thoughts, ideas, and opinions both spoken and internalized
+are the forces, that traverse all platforms, all standards
+and all norms to form their unique class, unique style, and a
+unique kind of ideology that future generations will look up to.
+We are #!/Shabang. (c) 2019/2020 Shabang Systems, LLC. All rights reserved
+unless explicitly stated otherwise or where it is prohibited by law */
+'''
